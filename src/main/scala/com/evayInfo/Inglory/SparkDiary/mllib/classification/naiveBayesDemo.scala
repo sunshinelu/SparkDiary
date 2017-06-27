72c5518bfb4100d059412159d186f17284996b2a
sunlu <sunlu900326@yeah.net>
2017-06-27 16:50:43 +0800

    pyhtonDemo

---
+++ src/main/scala/com/evayInfo/Inglory/SparkDiary/mllib/classification/naiveBayesDemo.scala
@@ -0,0 +1,50 @@
+package com.evayInfo.Inglory.SparkDiary.mllib.classification
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.classification.NaiveBayes
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by sunlu on 17/6/23.
+ */
+object naiveBayesDemo {
+  def main(args: Array[String]) {
+    //1 构建Spark对象
+    val conf = new SparkConf().setAppName("naive_bayes").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    //读取样本数据1
+    val data = sc.textFile("data/sample_naive_bayes_data.txt")
+    val parsedData = data.map { line =>
+      val parts = line.split(',')
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
+    }
+
+    //样本数据划分训练样本与测试样本
+    val splits = parsedData.randomSplit(Array(0.6, 0.4), seed = 11L)
+    val training = splits(0)
+    val test = splits(1)
+
+    //新建贝叶斯分类模型模型，并训练
+    val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+
+    //对测试样本进行测试
+    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+    val print_predict = predictionAndLabel.take(20)
+    println("prediction" + "\t" + "label")
+    for (i <- 0 to print_predict.length - 1) {
+      println(print_predict(i)._1 + "\t" + print_predict(i)._2)
+    }
+
+    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+    /*
+        //保存模型
+        val ModelPath = "result/naive_bayes_model"
+        model.save(sc, ModelPath)
+        val sameModel = NaiveBayesModel.load(sc, ModelPath)
+    */
+  }
+}
