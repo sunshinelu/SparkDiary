2144c1aafbaefa5073568b6a1025a431c1c237ae
sunlu <sunlu900326@yeah.net>
2017-06-27 17:28:44 +0800

    pyhtonDemo

--- src/main/python/com/evayInfo/Inglory/SparkDiary/LearnPython/BasicDemo/hello.py
+++ src/main/python/com/evayInfo/Inglory/SparkDiary/LearnPython/BasicDemo/hello.py
@@ -59,3 +59,13 @@ print t
 t[2][0] = "X"
 t[2][1] = "Y"
 print t
+
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum = sum + x
+print sum
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print sum
