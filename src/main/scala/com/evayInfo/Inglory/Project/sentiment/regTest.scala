2b3d18a24b7d2299a52eb83c8e3dade9d92cd04c
sunlu <sunlu900326@yeah.net>
2017-07-18 17:24:30 +0800

    regTest

--- src/main/scala/com/evayInfo/Inglory/Project/sentiment/regTest.scala
+++ src/main/scala/com/evayInfo/Inglory/Project/sentiment/regTest.scala
@@ -2,7 +2,9 @@ package com.evayInfo.Inglory.Project.sentiment
 
 /**
  * Created by sunlu on 17/7/18.
- * 参考链接
+ * 参考链接：
+ * https://www.zhihu.com/question/19816153
+ * http://blog.csdn.net/wqs1028/article/details/50388393
  */
 object regTest {
   def main(args: Array[String]) {
@@ -48,6 +50,7 @@ object regTest {
     val p6 = reg6.findAllMatchIn(ss).mkString(";")
     println("p6 is: " + p6)
     //提取结果为：//@何炅;//@李维嘉
+
     /*
        提取转发全部内容
         */
@@ -56,6 +59,31 @@ object regTest {
     val p7 = reg7.findAllMatchIn(ss).mkString(";")
     println("p7 is: " + p7)
     println(reg7.replaceAllIn(ss, ""))
+    /*
+    提取正文
+     */
+    val reg8 = "//@[\\u4e00-\\u9fa5a-zA-Z0-9_-]+:[\\u4e00-\\u9fa5a-zA-Z0-9_,.?:;'\"!，。！“”：；？]+|@[^,，：:\\s@]+|#[^#]+#".r
+    val p8 = reg8.replaceAllIn(s, "")
+    println("p8 is: " + p8)
+
+    /*
+    表情符号的替换
+     */
+
+    val sss = "#呵呵呵#[偷笑] http://test.com/blah_blah"
+    val reg9 = "\\[[0-9a-zA-Z\\u4e00-\\u9fa5]+\\]".r
+    val p9 = reg9.replaceAllIn(sss, "")
+    println("p9 is: " + p9)
+
+    /*
+    提取URL
+     */
+
+    val reg10 = "\\b(([\\w-]+://?|www[.])[^\\s()<>]+(?:\\([\\w\\d]+\\)|([^[:punct:]\\s]|/)))".r
+    val p10 = reg10.findAllMatchIn(sss).mkString(";")
+    println("p10 is: " + p10)
+
+
 
   }
 }
