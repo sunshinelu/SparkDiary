48f7009d86b8649559f05cf8e4c10aa8dcd2e603
sunlu <sunlu900326@yeah.net>
2017-07-29 18:21:58 +0800

    UDF function

---
+++ src/main/scala/com/evayInfo/Inglory/TestCode/udfTest.scala
@@ -0,0 +1,43 @@
+package com.evayInfo.Inglory.TestCode
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.udf
+
+/*
+ * Created by sunlu on 17/7/29
+ * 测试UDF
+ * 使用def定义函数的时候必须有参数才能在udf中使用。
+ */
+object udfTest {
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setAppName("udfTest").setMaster("local[*]")
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    val sc = spark.sparkContext
+    import spark.implicits._
+
+    val rdd1 = sc.parallelize(Seq((1,"a"),(2,"b"),(3,"c")))
+
+    val df1 = rdd1.toDF("col1", "col2")
+    df1.show()
+
+
+    val uuidUDF = udf((arg:String) => func1(arg))
+    val df2 = df1.withColumn("col3",uuidUDF($"col1"))
+    df2.show()
+
+    val uuidUDF2 = udf(func2())
+    val df3 = df1.withColumn("col3",uuidUDF2())
+    df3.show()
+
+  }
+  def func1(arg:String):String = {
+    "FuncTest1"
+  }
+
+  def func2():String = {
+    "FuncTest2"
+  }
+
+}
--- src/main/scala/com/evayInfo/Inglory/TestCode/uuidGenerate.scala
+++ src/main/scala/com/evayInfo/Inglory/TestCode/uuidGenerate.scala
@@ -2,6 +2,9 @@ package com.evayInfo.Inglory.TestCode
 
 import java.util.UUID
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 /**
  * Created by sunlu on 17/7/27.
  * 使用rt.jar生成UUID
@@ -10,5 +13,36 @@ object uuidGenerate {
   def main(args: Array[String]) {
     val s1 = UUID.randomUUID().toString().toLowerCase()
     println(s1)
+
+    val sparkConf = new SparkConf().setAppName("uuidGenerate").setMaster("local[*]")
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    val sc = spark.sparkContext
+    import spark.implicits._
+    val rdd1 = sc.parallelize(Seq((1,"a"),(2,"b"),(3,"c")))
+
+    val df1 = rdd1.toDF("col1", "col2")
+    df1.show()
+
+
+    val uuidUDF = udf((arg:String) => uuidFunc(arg))
+    val df2 = df1.withColumn("col3",uuidUDF($"col1"))
+    df2.show()
+
+    val uuidUDF2 = udf(uuidFunc2())
+    val df3 = df1.withColumn("col3",uuidUDF2())
+    df3.show()
+
+    sc.stop()
+    spark.stop()
   }
+  def uuidFunc(arg:String):String={
+    val uuid = UUID.randomUUID().toString().toLowerCase()
+    uuid
+  }
+
+  def uuidFunc2():String={
+    val uuid = UUID.randomUUID().toString().toLowerCase().toString
+    uuid
+  }
+
 }
