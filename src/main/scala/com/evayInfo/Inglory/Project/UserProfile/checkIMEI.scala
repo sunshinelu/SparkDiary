f10d00346487741cdbc02099cc7b66ac42492441
sunlu <sunlu900326@yeah.net>
2017-12-05 09:19:35 +0800

    checkIMEI

---
+++ src/main/scala/com/evayInfo/Inglory/Project/UserProfile/checkIMEI.scala
@@ -0,0 +1,98 @@
+package com.evayInfo.Inglory.Project.UserProfile
+
+import java.text.SimpleDateFormat
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.protobuf.ProtobufUtil
+import org.apache.hadoop.hbase.util.{Base64, Bytes}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by sunlu on 17/12/5.
+ * 查看
+ */
+object checkIMEI {
+
+  def convertScanToString(scan: Scan) = {
+    val proto = ProtobufUtil.toScan(scan)
+    Base64.encodeBytes(proto.toByteArray)
+  }
+
+  def SetLogger = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("com").setLevel(Level.OFF)
+    System.setProperty("spark.ui.showConsoleProgress", "false")
+    Logger.getRootLogger().setLevel(Level.OFF)
+  }
+
+  case class LogView(CREATE_BY_ID: String, CREATE_TIME_L: Long, CREATE_TIME: String, REQUEST_URI: String, PARAMS: String)
+
+  def getLogsRDD(logsTable: String, sc: SparkContext): RDD[LogView] = {
+
+    val conf = HBaseConfiguration.create() //在HBaseConfiguration设置可以将扫描限制到部分列，以及限制扫描的时间范围
+    //设置查询的表名
+    conf.set(TableInputFormat.INPUT_TABLE, logsTable) //设置输入表名 第一个参数yeeso-test-ywk_webpage
+
+    //扫描整个表中指定的列和列簇
+    val scan = new Scan()
+    scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("cREATE_BY_ID")) //cREATE_BY_ID
+    scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("cREATE_TIME")) //cREATE_TIME
+    scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("rEQUEST_URI")) //rEQUEST_URI
+    scan.addColumn(Bytes.toBytes("info"), Bytes.toBytes("pARAMS")) //pARAMS
+    conf.set(TableInputFormat.SCAN, convertScanToString(scan))
+
+    val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
+      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+      classOf[org.apache.hadoop.hbase.client.Result])
+    //提取hbase数据，并对数据进行过滤
+    val hbaseRDD = hBaseRDD.map { case (k, v) => {
+      val rowkey = k.get()
+      val userID = v.getValue(Bytes.toBytes("info"), Bytes.toBytes("cREATE_BY_ID")) //cREATE_BY_ID
+      val creatTime = v.getValue(Bytes.toBytes("info"), Bytes.toBytes("cREATE_TIME")) //cREATE_TIME
+      val requestURL = v.getValue(Bytes.toBytes("info"), Bytes.toBytes("rEQUEST_URI")) //rEQUEST_URI
+      val parmas = v.getValue(Bytes.toBytes("info"), Bytes.toBytes("pARAMS")) //pARAMS
+      (userID, creatTime, requestURL, parmas)
+    }
+    }.filter(x => null != x._1 & null != x._2 & null != x._3 & null != x._4).
+      map { x => {
+        val userID = Bytes.toString(x._1)
+        val creatTime = Bytes.toString(x._2)
+        //定义时间格式
+        val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss") // yyyy-MM-dd HH:mm:ss或者 yyyy-MM-dd
+        val dateFormat2 = new SimpleDateFormat("yyyy-MM-dd") // yyyy-MM-dd HH:mm:ss或者 yyyy-MM-dd
+        val creatTimeD = dateFormat.parse(creatTime)
+        val creatTimeS = dateFormat2.format(creatTimeD)
+        val creatTimeL = dateFormat2.parse(creatTimeS).getTime
+        val requestURL = Bytes.toString(x._3)
+        val parmas = Bytes.toString(x._4)
+        LogView(userID, creatTimeL, creatTimeS, requestURL, parmas)
+      }
+      }
+
+    hbaseRDD
+  }
+
+  def main(args: Array[String]) {
+    SetLogger
+
+    //bulid environment
+    val SparkConf = new SparkConf().setAppName(s"checkIMEI").setMaster("local[*]").set("spark.executor.memory", "2g")
+    val spark = SparkSession.builder().config(SparkConf).getOrCreate()
+    val sc = spark.sparkContext
+
+    // 获取日志数据
+    val logsTable = "t_hbaseSink"
+    val logsRDD = getLogsRDD(logsTable, sc)
+    val logsDS = spark.createDataset(logsRDD).select("CREATE_BY_ID","REQUEST_URI", "PARAMS")
+    logsDS.show(false)
+
+    sc.stop()
+    spark.stop()
+  }
+
+}
