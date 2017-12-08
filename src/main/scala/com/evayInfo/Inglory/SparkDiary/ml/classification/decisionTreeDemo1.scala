d5c832512f3c5943c449a4cf634b372f5fb3587d
sunlu <sunlu900326@yeah.net>
2017-12-08 10:32:42 +0800

    decisionTreeDemo1

---
+++ src/main/scala/com/evayInfo/Inglory/SparkDiary/ml/classification/decisionTreeDemo1.scala
@@ -0,0 +1,92 @@
+package com.evayInfo.Inglory.SparkDiary.ml.classification
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+
+/**
+ * Created by sunlu on 17/12/8.
+ */
+object decisionTreeDemo1 {
+  def SetLogger = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("com").setLevel(Level.OFF)
+    System.setProperty("spark.ui.showConsoleProgress", "false")
+    Logger.getRootLogger().setLevel(Level.OFF)
+  }
+
+  def main(args: Array[String]) {
+    //bulid environment
+    val SparkConf = new SparkConf().setAppName(s"decisionTreeDemo1").setMaster("local[*]").set("spark.executor.memory", "2g")
+    val spark = SparkSession.builder().config(SparkConf).getOrCreate()
+    val sc = spark.sparkContext
+    import spark.implicits._
+
+    // Load the data stored in LIBSVM format as a DataFrame.
+    val data = spark.read.format("libsvm").load("file:///Users/sunlu/Documents/workspace/IDEA/SparkDiary/data/sample_libsvm_data.txt")
+
+    data.printSchema()
+
+    // Index labels, adding metadata to the label column.
+    // Fit on whole dataset to include all labels in index.
+    val labelIndexer = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+      .fit(data)
+    // Automatically identify categorical features, and index them.
+    val featureIndexer = new VectorIndexer()
+  .setInputCol("features")
+  .setOutputCol("indexedFeatures")
+  .setMaxCategories(4) // features with > 4 distinct values are treated as continuous.
+  .fit(data)
+
+    // Split the data into training and test sets (30% held out for testing).
+    val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
+    // Train a DecisionTree model.
+    val dt = new DecisionTreeClassifier()
+      .setLabelCol("indexedLabel")
+      .setFeaturesCol("indexedFeatures")
+
+    // Convert indexed labels back to original labels.
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("predictedLabel")
+      .setLabels(labelIndexer.labels)
+
+    // Chain indexers and tree in a Pipeline.
+    val pipeline = new Pipeline()
+      .setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
+
+    // Train model. This also runs the indexers.
+    val model = pipeline.fit(trainingData)
+
+    // Make predictions.
+    val predictions = model.transform(testData)
+
+    // Select example rows to display.
+    predictions.select("predictedLabel", "label", "features").show(5)
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("indexedLabel")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println("Test Error = " + (1.0 - accuracy))
+
+    val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
+    println("Learned classification tree model:\n" + treeModel.toDebugString)
+
+
+    sc.stop()
+    spark.stop()
+
+  }
+
+}
---
+++ src/main/scala/com/evayInfo/Inglory/SparkDiary/ml/classification/decisionTreeDemo2.scala
@@ -0,0 +1,8 @@
+package com.evayInfo.Inglory.SparkDiary.ml.classification
+
+/**
+ * Created by sunlu on 17/12/8.
+ */
+object decisionTreeDemo2 {
+
+}
