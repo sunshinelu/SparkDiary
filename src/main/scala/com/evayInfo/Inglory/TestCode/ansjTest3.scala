b8faf18ac13434147e2653ece87a55a551e489bd
sunlu <sunlu900326@yeah.net>
2017-12-20 13:50:11 +0800

    ansjTest

--- src/main/scala/com/evayInfo/Inglory/TestCode/ansjTest3.scala
+++ src/main/scala/com/evayInfo/Inglory/TestCode/ansjTest3.scala
@@ -1,6 +1,7 @@
 package com.evayInfo.Inglory.TestCode
 
-import org.ansj.splitWord.analysis.DicAnalysis
+import org.ansj.library.UserDefineLibrary
+import org.ansj.splitWord.analysis.{IndexAnalysis, DicAnalysis}
 import org.ansj.util.FilterModifWord
 
 /**
@@ -14,6 +15,13 @@ object ansjTest3 {
     println(ansj)
     val result = FilterModifWord.modifResult(ansj)
     println(result)
+
+    // 索引分词
+    UserDefineLibrary.insertWord("山东亿云信息技术有限公司", "userDefine", 1000)
+    UserDefineLibrary.insertWord("亿云", "userDefine", 1000)
+    UserDefineLibrary.insertWord("亿云信息", "userDefine", 1000)
+    val parse = IndexAnalysis.parse("山东亿云信息技术有限公司")
+    println(parse)
   }
 
 }
