2b013f286a006101d4fa0b565fae506f6e019cbb
sunlu <sunlu900326@yeah.net>
2017-12-20 21:31:23 +0800

    MultilayerPerceptronClassifier

--- src/main/scala/com/evayInfo/Inglory/SparkDiary/ml/classification/decisionTreeDemo3.scala
+++ src/main/scala/com/evayInfo/Inglory/SparkDiary/ml/classification/decisionTreeDemo3.scala
@@ -1,20 +1,16 @@
 package com.evayInfo.Inglory.SparkDiary.ml.classification
 
+import com.evayInfo.Inglory.SparkDiary.ml.classification.SpamMessageClassifier.SetLogger
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, Word2Vec}
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
-/**
- * Created by sunlu on 17/12/20.
- * 《图解Spark》第九章Spark MLlib（P338）
- * http://mangocool.com/1476675638181.html
- * https://stackoverflow.com/questions/38664972/why-is-unable-to-find-encoder-for-type-stored-in-a-dataset-when-creating-a-dat
- */
-object SpamMessageClassifier {
+object decisionTreeDemo3 {
 
   def SetLogger = {
     Logger.getLogger("org").setLevel(Level.OFF)
@@ -23,9 +19,7 @@ object SpamMessageClassifier {
     Logger.getRootLogger().setLevel(Level.OFF)
   }
 
-
-  def main(args: Array[String]) {
-
+  def main(args: Array[String]): Unit = {
     SetLogger
 
     //bulid environment
@@ -45,14 +39,21 @@ object SpamMessageClassifier {
       setInputCol("labelCol").
       setOutputCol("indexedLabelCol").
       fit(smsDF)
+    val labelIndexerDF = labelIndexer.transform(smsDF)
 
     // 使用word2vec将文本转化为数值型词向量
     val word2vec = new Word2Vec().
       setInputCol("contextCol").
       setOutputCol("featuresCol").
       setVectorSize(100).
-      setMaxIter(1)
+      setMaxIter(1).fit(labelIndexerDF)
+    val toVecUDF  = udf((arg:Double) => Vectors.dense(Array(arg)))
+    val toVecUDF2  = udf((arg:Double) => Vectors.dense(arg))
+    val word2vecDF = word2vec.transform(labelIndexerDF)//.withColumn("indexedLabelCol", toVecUDF($"indexedLabelCol"))
+    word2vecDF.printSchema()
+  word2vecDF.show()
 
+    val test = word2vecDF.select("indexedLabelCol", "featuresCol")
     val layers = Array[Int](100, 6,5,2)
 
     // 使用 MultilayerPerceptronClassifier 训练一个多层感知器模型
@@ -63,34 +64,23 @@ object SpamMessageClassifier {
       setMaxIter(128).
       setFeaturesCol("indexedLabelCol").
       setPredictionCol("predictionCol")
+      val mpcModel = mpc.fit(test)
+    val mpcDF = mpcModel.transform(test)
 
     // 使用 LabelConverter 将预测结果的数值标签转换成原始的文本标签
     val labelConverter = new IndexToString().
       setInputCol("predictionCol").
       setOutputCol("predictedLabelCol").
       setLabels(labelIndexer.labels)
+    val labelConverterDF = labelConverter.transform(mpcDF)
+    labelConverterDF.printSchema()
+
 
     // 将原始文本数据按照 8:2 的比例分成训练集和测试集
-    val Array(trainingData, testData) = smsDF.randomSplit(Array(0.8, 0.2))
-
-    // 使用 pipeline 对数据进行处理和模型训练
-    val pipeline  = new Pipeline().setStages(Array(labelIndexer, word2vec, mpc, labelConverter))
-    val model = pipeline.fit(trainingData)
-    val preResultDF = model.transform(testData)
-
-    // 使用模型对预测数据进行分类处理并在屏幕上打印20笔数据
-    preResultDF.select("contextCol", "labelCol", "predictedLabelCol").show(20)
-
-    // 测试数据集上测试模型的预测精确度
-    val evaluator = new MulticlassClassificationEvaluator().
-      setLabelCol("indexedLabelCol").
-      setPredictionCol("predictionCol")
-
-    val predictionAccuracy = evaluator.evaluate(preResultDF)
-
-    println("Testing Accuracy is %2.4f".format(predictionAccuracy * 100) + "%")
+//    val Array(trainingData, testData) = smsDF.randomSplit(Array(0.8, 0.2))
 
     sc.stop()
     spark.stop()
   }
+
 }
---
+++ src/main/scala/com/evayInfo/Inglory/SparkDiary/ml/classification/mpcDemo1.scala
@@ -0,0 +1,66 @@
+package com.evayInfo.Inglory.SparkDiary.ml.classification
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+object mpcDemo1 {
+  def SetLogger = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("com").setLevel(Level.OFF)
+    System.setProperty("spark.ui.showConsoleProgress", "false")
+    Logger.getRootLogger().setLevel(Level.OFF)
+  }
+
+  def main(args: Array[String]): Unit = {
+    SetLogger
+
+    //bulid environment
+    val SparkConf = new SparkConf().setAppName(s"mpcDemo1").setMaster("local[*]").set("spark.executor.memory", "4g")
+    val spark = SparkSession.builder().config(SparkConf).getOrCreate()
+    val sc = spark.sparkContext
+    import spark.implicits._
+
+    val filePath = "file:///D:\\Workspace\\IDEA\\GitHub\\SparkDiary\\data\\sample_multiclass_classification_data.txt"
+
+    // Load the data stored in LIBSVM format as a DataFrame.
+    val data = spark.read.format("libsvm")
+      .load(filePath)
+    data.printSchema()
+    data.show()
+
+    // Split the data into train and test
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+    val train = splits(0)
+    val test = splits(1)
+
+    // specify layers for the neural network:
+    // input layer of size 4 (features), two intermediate of size 5 and 4
+    // and output of size 3 (classes)
+    val layers = Array[Int](4, 5, 4, 3)
+
+    // create the trainer and set its parameters
+    val trainer = new MultilayerPerceptronClassifier()
+      .setLayers(layers)
+      .setBlockSize(128)
+      .setSeed(1234L)
+      .setMaxIter(100)
+
+    // train the model
+    val model = trainer.fit(train)
+
+    // compute accuracy on the test set
+    val result = model.transform(test)
+    val predictionAndLabels = result.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+
+    println("Test set accuracy = " + evaluator.evaluate(predictionAndLabels))
+    sc.stop()
+    spark.stop()
+
+  }
+
+}
