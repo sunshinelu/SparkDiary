8c1ea2e361181946a82a3f669b40aedcf690fc29
sunlu <sunlu900326@yeah.net>
2018-11-05 15:27:31 +0800

    数据挖掘分析平台：推荐算法模型

--- src/main/java/com/evayInfo/Inglory/Project/DataMiningPlatform/machine/learning/recommend/BuildItemBasedModelTest1.java
+++ src/main/java/com/evayInfo/Inglory/Project/DataMiningPlatform/machine/learning/recommend/BuildItemBasedModelTest1.java
@@ -4,4 +4,7 @@ package com.evayInfo.Inglory.Project.DataMiningPlatform.machine.learning.recomme
  * Created by sunlu on 18/10/31.
  */
 public class BuildItemBasedModelTest1 {
+    public static void main(String[] args) {
+
+    }
 }
